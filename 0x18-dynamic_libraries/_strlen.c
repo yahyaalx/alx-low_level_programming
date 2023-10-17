1433463933736a3d659af8ba056837b26645794c
yahyaalx <colo9irch@gmail.com>
2023-10-17 03:11:38 +0100

    win

--- 0x18-dynamic_libraries/_strlen.c
+++ 0x18-dynamic_libraries/_strlen.c
@@ -2,16 +2,14 @@
 /**
  * _strlen - length of a string
  * @s: string
- * Return : length of string 
+ * @return : length of string
  */
 int _strlen(char *s)
 {
 	int	i;
 
 	i = 0;
-
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 		i++;
 	return (i);
 }
-
