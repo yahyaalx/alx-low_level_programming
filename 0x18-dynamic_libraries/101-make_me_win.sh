f365e36fb52d6e614a1caebaa40299b71564dabe
yahyaalx <colo9irch@gmail.com>
2023-10-17 03:30:51 +0100

    llllssssssssssss

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -P /tmp\
-       	https://github.com/yahyaalx/alx-low_level_programming/blob/main/0x18-dynamic_libraries/test.so
+wget -P /tmp https://github.com/yahyaalx/alx-low_level_programming/blob/main/0x18-dynamic_libraries/test.so
 export LD_PRELOAD=/tmp/test.so
