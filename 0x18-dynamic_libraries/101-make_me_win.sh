2466a3d0e8ec85b1e61fd3f8160bf67421df43eb
yahyaalx <colo9irch@gmail.com>
2023-10-21 03:26:46 +0100

    make

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/yahyaalx/alx-low_level_programming/blob/main/0x18-dynamic_libraries/test.so
-export LD_PRELOAD=/tmp/test.so
+wget -P /tmp https://github.com/yahyaalx/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libertest.so
+export LD_PRELOAD=/$PWD/libertest.so
